fa314f2e3ccadd6a426245069132ec2fddee8a32
Ronie Soares <91928250+Ronie-Soares@users.noreply.github.com>
2024-11-09 20:40:54 -0300

    teste2
    teste github app

--- src/main/java/qbank/sistemabancario/Application.java
+++ src/main/java/qbank/sistemabancario/Application.java
@@ -10,7 +10,7 @@ public class Application {
     public static void main(String[] args) {
         Pessoa pessoaTeste = new Pessoa("Ronie Soares","00100200355","12345678","ronie.profissional@gmail.com","31993143539");
         Conta conta1 = new ContaCorrente("0001","123456-7", 1000.00F,pessoaTeste);
-//
+//        adicionando comentario teste
 //        System.out.println("TESTE: "+ conta1.DadosConta());
 //
 //        Float saldo = conta1.getSaldo();
