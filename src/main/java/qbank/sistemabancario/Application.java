3c206b1eb12dd52dbff5c1c50c5f115318c01777
Rafael Custódio <rafa.gsc7@gmail.com>
2024-11-09 16:33:35 -0300

    Teste
    Comentários alterados para teste

--- src/main/java/qbank/sistemabancario/Application.java
+++ src/main/java/qbank/sistemabancario/Application.java
@@ -8,12 +8,12 @@ import qbank.sistemabancario.context.pessoa.Pessoa;
 public class Application {
 
     public static void main(String[] args) {
-//        Pessoa pessoaTeste = new Pessoa("Ronie Soares","00100200355","12345678","ronie.profissional@gmail.com","31993143539");
-//        Conta conta1 = new ContaCorrente("0001","123456-7", 1000.00F,pessoaTeste);
-//
-//        System.out.println("TESTE: "+ conta1.DadosConta());
-//
-//        Float saldo = conta1.getSaldo();
+        Pessoa pessoaTeste = new Pessoa("Ronie Soares","00100200355","12345678","ronie.profissional@gmail.com","31993143539");
+        Conta conta1 = new ContaCorrente("0001","123456-7", 1000.00F,pessoaTeste);
+
+        System.out.println("TESTE: "+ conta1.DadosConta());
+
+        Float saldo = conta1.getSaldo();
         Micronaut.run(Application.class, args);
     }
 }
