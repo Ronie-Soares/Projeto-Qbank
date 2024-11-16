5301e5794b99032dd4bc3993cb090d47eb6ab092
Eliseu Silva <eueliseeu@gmail.com>
2024-11-16 13:48:18 -0300

    Adiciona identificação de conta Poupança para integração com JSON

--- src/main/java/qbank/sistemabancario/context/conta/ContaPoupança.java
+++ src/main/java/qbank/sistemabancario/context/conta/ContaPoupança.java
@@ -2,15 +2,15 @@ package qbank.sistemabancario.context.conta;
 
 import qbank.sistemabancario.context.pessoa.Pessoa;
 
-public class ContaPoupança extends Conta{
+import java.math.BigDecimal;
 
+public class ContaPoupança extends Conta {
     public ContaPoupança(String agencia, String numero, Float saldo, Pessoa pessoa) {
-        super(agencia, numero, saldo, pessoa);
+        super(agencia, numero, BigDecimal.valueOf(saldo), pessoa, "Poupança");
     }
 
     @Override
-    public Float getSaldo() {
-        System.out.println("Saldo da sua conta Poupança: ");
+    public BigDecimal getSaldo() {
         return super.getSaldo();
     }
 }
