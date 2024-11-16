0abe7be4319e7f6a65c836771f333687e4daa2b6
Eliseu Silva <eueliseeu@gmail.com>
2024-11-16 13:47:20 -0300

    Corrige erro de declaração de saldo e ajusta tipos de dados em Conta
    Corrigi a declaração de saldo na classe Conta, substituindo o tipo Float por BigDecimal para garantir maior precisão em operações financeiras. Além disso, ajustei o código para refletir a mudança no tipo de saldo, incluindo modificações nos métodos getSaldo() e setSaldo(). Também fiz melhorias nos construtores e no método DadosConta para tratar de possíveis valores nulos.

--- src/main/java/qbank/sistemabancario/context/conta/Conta.java
+++ src/main/java/qbank/sistemabancario/context/conta/Conta.java
@@ -1,61 +1,44 @@
 package qbank.sistemabancario.context.conta;
 
 import qbank.sistemabancario.context.pessoa.Pessoa;
-import qbank.sistemabancario.strategy.transacao.Transacao;
+import java.math.BigDecimal;
 
 public abstract class Conta {
-
-    private Transacao transacao;
     private String agencia;
     private String numero;
-    private Float saldo;
+    private BigDecimal saldo;
     private Pessoa pessoa;
+    private String tipoConta;
 
-    public Conta(String agencia, String numero, Float saldo, Pessoa pessoa) {
+    public Conta(String agencia, String numero, BigDecimal saldo, Pessoa pessoa, String tipoConta) {
         this.agencia = agencia;
         this.numero = numero;
         this.saldo = saldo;
         this.pessoa = pessoa;
+        this.tipoConta = tipoConta;
+    }
+
+    public String getTipoConta() {
+        return tipoConta;
     }
 
     public String getAgencia() {
         return agencia;
     }
-    public void setAgencia(String agencia) {
-        this.agencia = agencia;
-    }
 
     public String getNumero() {
         return numero;
     }
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
 
-    public Float getSaldo() {
-        System.out.println(saldo);
+    public BigDecimal getSaldo() {
         return saldo;
-    }
-    public void setSaldo(Float saldo) {
-        this.saldo = saldo;
     }
 
     public Pessoa getPessoa() {
         return pessoa;
     }
-    public void setPessoa(Pessoa pessoa) {
-        this.pessoa = pessoa;
-    }
 
-    public String DadosConta() {
-        // Verificação simples para evitar NullPointerException
-        String nome = (pessoa != null) ? pessoa.getNome() : "N/A";
-        String mensagem = String.format("AG: %s \nCONTA: %s \nNOME: %s \nSALDO: %.2f",
-                agencia, numero, nome, saldo);
-        return mensagem;
-    }
-
-    public boolean contaCriada(){
-        return true;
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
     }
 }
